6aa23e682f3318908de0ff439fb45705eee56062
Bodea Mihai Cristian <bodeamc@gmail.com>
2023-01-05 16:56:02 +0200

    First version our my website

---
+++ index2.js
@@ -0,0 +1,5 @@
+var skillsEl = document.getElementById("skills-list");
+
+skillsEl.innerHTML += '<li class = "favorite">HTML</li>';
+skillsEl.innerHTML += '<li class = "favorite">CSS</li>';
+skillsEl.innerHTML += "<li>JS</li>";
