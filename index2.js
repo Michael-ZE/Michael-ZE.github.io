2bfb07e4f3a2aa9b1fb7fa143fcc3489f41af958
Bodea Mihai Cristian <bodeamc@gmail.com>
2023-01-05 21:03:57 +0200

    Display on click change

--- index2.js
+++ index2.js
@@ -10,4 +10,32 @@ for (var i=1; i<skills.length; i++) {
 
 skillsEl.innerHTML = skillsHTML;
 
+// display only home
+function displayHome(){
+    document.getElementById("home").style.display="block";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("languages").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+}
+function displaySkills(){
+    //console.warn("trebuie sa afisam DOAR skills page");
+    document.getElementById("home").style.display = "none";
+    document.getElementById("skills").style.display = "block";
+    document.getElementById("languages").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+}
 
+function displayLanguages(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("languages").style.display = "block";
+    document.getElementById("projects").style.display = "none";
+}
+
+function displayProjects(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("languages").style.display = "none";
+    document.getElementById("projects").style.display = "block";
+}
+displayHome();
