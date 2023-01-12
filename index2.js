e93f854b06a2ba6a06723817d233672dc1fc6e80
Bodea Mihai Cristian <bodeamc@gmail.com>
2023-01-12 19:03:16 +0200

    add hide function

--- index2.js
+++ index2.js
@@ -10,13 +10,37 @@ for (var i=1; i<skills.length; i++) {
 
 skillsEl.innerHTML = skillsHTML;
 
+function hideAllPages() {
+    hide("home");
+    hide("skills");
+    hide("languages");
+    hide("projects");
+} 
+
+function show(id){
+    document.getElementById(id).style.display="block";
+}
+
+function hide(id){
+ document.getElementById(id).style.display="none";
+}
+
+
+
 // display only home
-function displayHome(){
+//function displayHome(){
     document.getElementById("home").style.display="block";
+    // sau scriu doar: hideAllPages(); 
+    // in loc de "display = none" de 3 ori
     document.getElementById("skills").style.display = "none";
     document.getElementById("languages").style.display = "none";
-    document.getElementById("projects").style.display = "none";
-}
+    document.getElementById("projects").style.display = "none";}
+
+function displayHome(){
+    hideAllPages();
+    show("home");
+}    
+
 function displaySkills(){
     //console.warn("trebuie sa afisam DOAR skills page");
     document.getElementById("home").style.display = "none";
